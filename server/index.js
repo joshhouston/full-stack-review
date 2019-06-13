cfbedfbd7b71e325bb7b7deee8a0eddfc5f1ce3b
josh <jjhouston24@gmail.com>
2019-06-13 14:10:26 -0500

    registration-working

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const massive = require('massive');
 const session = require('express-session');
+const {registerUser, loginUsers, getUser, logOut} = require('./controllers/authController');
 
 
 const app = express();
@@ -21,6 +22,11 @@ app.use(
     })
 )
 
+//endpoints
+app.post('/auth/register', registerUser)
+app.post('/auth/login', loginUsers)
+app.get('/auth/user', getUser)
+app.post('/auth/logout', logOut)
 
 massive(process.env.CONNECTION_STRING).then(db => {
     app.set('db', db)
--- src/components/authorization/Landing.js
+++ src/components/authorization/Landing.js
@@ -1,10 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
 
 class Landing extends Component {
-    render(){
+    render() {
         return (
             <div>
-                Landing
+                <Link to='/login'>
+                    <button>Login</button>
+                </Link>
+                <Link to='/register'>
+                    <button>Create Account</button>
+                </Link>
+                <Link>
+                    <button>Logout</button>
+                </Link>
             </div>
         )
     }
